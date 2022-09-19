9d5b7adb65c31663636e3e745d5fd36a0c3b570f
ramazansen1 <you@example.com>
2022-09-19 10:29:30 +0300

    19092022-1029

---
+++ BinarySearchTree.py
@@ -0,0 +1,15 @@
+"""Proje 3
+[7, 5, 1, 8, 3, 6, 0, 9, 4, 2] dizisinin Binary-Search-Tree aşamalarını yazınız.
+
+Örnek: root x'dir. root'un sağından y bulunur. Solunda z bulunur vb."""
+
+"""Cevap
+
+root 5'dir.
+
+5'in solunda 3, sağında 7 bulunur.
+    3'ün solunda 1, sağında 4 bulunur.
+        1'in solunda 0, sağında 2 bulunur.
+
+    7'in solunda 6, sağında 8 bulunur.
+        8'in sağında 9 bulunur."""
---
+++ MengeSort.py
@@ -0,0 +1,15 @@
+"""Proje 2
+[16,21,11,8,12,22] -> Merge Sort
+
+1-)Yukarıdaki dizinin sort türüne göre aşamalarını yazınız.
+2-)Big-O gösterimini yazınız."""
+
+"""Cevap-1)
+Adım1 -- [16,21,11] -- [8,12,22]
+Adım2 -- [16,21],[11] -- [8,12],[22]
+Adım3 -- [11,16,21] -- [8,12,22]
+Adım4 -- [8,11,12,16,21,22]
+
+Cevap-2)
+O(nlog(n))
+"""
